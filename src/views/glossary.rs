57654145d8e24da79f5e3681697317d58f2ebd57
Tomas Maceira <106024929+ARGENTOS72@users.noreply.github.com>
2025-03-26 08:33:20 +0100

    Add files via upload

--- src/views/glossary.rs
+++ src/views/glossary.rs
@@ -41,16 +41,16 @@ use crate::backend::glossary::get_glossary_file_rsxed;
 // }
 
 #[component]
-pub fn Glossary(chapter: String) -> Element {
+pub fn Glossary(chapter: Signal<String>) -> Element {
     let chapters = use_server_future(move || async move { get_chapters().await })?;
 
     dbg!(chapters.value());
 
     rsx! {
-        // GlossaryNavigation {
-        //     chapters: chapters.value()().unwrap().unwrap(),
-        //     current_chapter: chapter,
-        // }
+        GlossaryNavigation {
+            chapters: chapters.value()().unwrap().unwrap(), // Può dare problemi in caso commentare tutta la funzione
+            current_chapter: chapter,
+        },
         div {
             class: "p-4",
             style: "width: 75%; overflow: auto;",
