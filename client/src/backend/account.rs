0597057542b935e21fe22584968e6e3773db9afb
comuchlo <103433630+comuchlo@users.noreply.github.com>
2025-05-31 12:50:54 +0200

    json bohb  nnn va

--- client/src/backend/account.rs
+++ client/src/backend/account.rs
@@ -1,10 +1,9 @@
 use crate::backend::db::get_db;
 use crate::error::{Error, Result};
-use bcrypt::hash;
+use dioxus::Ok;
 use serde::{Deserialize, Serialize};
 use std::fs::OpenOptions;
 use std::io::{Read, Write};
-use surrealdb::Error as SurrealError;
 use tokio::sync::OnceCell;
 
 #[derive(Debug, Serialize, Deserialize, Clone, Default)]
@@ -34,47 +33,45 @@ pub async fn login(login: String, password: String) -> surrealdb::Result<User> {
         .bind(("password", hashedPSW.clone()))
         .await?;
 
-    let ress: Option<User> = res.take(0).unwrap();
+    let ress: Option<User> = res.take(0).unwrap()?;
 
     if let Some(user) = ress.clone() {
         USER.set(Some(user.clone())).unwrap();
-        write_pwd_on_file(hashedPSW);
+        write_pwd_on_file(hashedPSW, user.login.clone().unwrap());
     }
 
     Ok(ress.unwrap_or_default())
 }
 
 pub async fn try_login() -> Result<()> {
-    let user = USER
-        .get()
-        .ok_or(Error::Custom("User not initialized".to_string()))?
-        .as_ref()
-        .ok_or(Error::Custom("No user logged in".to_string()))?;
-
-    if let Some(pwd) = &user.password {
-        _validate_psswd(pwd.clone()).await?;
-    }
-    //da finirre
-
-    Ok(())
-}
-
-async fn _validate_psswd(pass: String) -> Result<()> {
-    // Add actual validation logic
     let db = get_db().await;
-
-    let mut resPSW = db
-        .query("RETURN crypto::bcrypt::compare($hash, $pass);") //query a caso da rimpiazzare (serveil login also)
-        .bind(("hash", hash))
-        .bind(("pass", pass))
+    let user: User = USER.get().unwrap_or(&Some(_get_psswrd_from_file().await.as_ref().unwrap().clone())).as_ref().unwrap().clone();
+    
+        
+    if let Some(pwd) = &user.password {
+        let mut res = db
+        .query("SELECT login, password FROM user WHERE login = $login AND password = $password")
+        .bind(("login", user.login.clone().unwrap()))
+        .bind(("password", user.password.clone().unwrap()))
         .await?;
-    let ressPSW: Option<String> = resPSW.take(0).unwrap();
-    let hashedPSW: String = ressPSW.unwrap_or("".to_string().clone());
+
+        let ress: Option<User> = res.take(0).unwrap();
+        return if let Some(user) = ress.clone() {
+            USER.set(Some(user.clone())).unwrap();
+            write_pwd_on_file(user.password.clone().unwrap(), user.login.clone().unwrap());
+            Ok(())
+        } else {
+            Err(Error::Custom("failed automatic login".to_string()))
+        }
+    }
+
+        Err(Error::Custom("failed automatic login".to_string()))
     //da finirre
-    Ok(())
+
 }
 
-async fn _get_psswrd_from_file() -> Result<String> {
+
+async fn _get_psswrd_from_file() -> Result<User> {
     let mut buf = String::new();
     let mut file = OpenOptions::new()
         .read(true)
@@ -85,18 +82,24 @@ async fn _get_psswrd_from_file() -> Result<String> {
     file.read_to_string(&mut buf)
         .map_err(|e| Error::Custom(format!("Read error: {}", e)))?;
 
-    Ok(buf)
+    let u:User= serde_json::from_str(&buf)?;
+
+    Ok(u)
 }
 
-async fn write_pwd_on_file(psw: String) -> Result<()> {
+async fn write_pwd_on_file(psw: String, lg: String) -> Result<()> {
     let mut file = OpenOptions::new()
         .write(true)
         .create(true)
         .open("/cookie/login.txt") //serve anche il login quindi va fatto conjson o boh explode nn lo so
         .map_err(|e| Error::Custom(format!("File error: {}", e)))?;
 
-    file.write_all(psw.as_bytes())
-        .map_err(|e| Error::Custom(format!("Write error: {}", e)))?;
+    let u:User= User {login: Some(lg.to_owned()), password: Some(psw.to_owned())};
+
+    let buf= serde_json::to_string(&u)?;
+
+    file.write_all(buf.as_bytes())
+    .map_err(|e| Error::Custom(format!("Write error: {}", e)))?;
 
     Ok(())
 }
